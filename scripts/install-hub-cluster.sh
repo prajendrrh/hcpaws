8b618ec54ed3a736228e8e47815d67f8d074154e
prajendrrh <prajendr@redhat.com>
2025-10-29 16:00:41 +0000

    Improve progress indicator: show dots every minute and message every 5 minutes

--- scripts/install-hub-cluster.sh
+++ scripts/install-hub-cluster.sh
@@ -73,11 +73,21 @@ else
     openshift-install create cluster --dir . --log-level debug > "$INSTALL_LOG" 2>&1 &
     INSTALL_PID=$!
     
-    # Show periodic status updates
+    # Show periodic status updates with dots and messages
+    COUNTER=0
+    echo -n "   Installing"
     while kill -0 $INSTALL_PID 2>/dev/null; do
         sleep 60
-        echo "   Still installing... (check $INSTALL_LOG for details)"
+        COUNTER=$((COUNTER + 1))
+        echo -n "."
+        # Show message every 5 minutes
+        if [ $((COUNTER % 5)) -eq 0 ]; then
+            echo ""
+            echo "   Still installing... ($((COUNTER)) minutes elapsed - check $INSTALL_LOG for details)"
+            echo -n "   Installing"
+        fi
     done
+    echo ""
     wait $INSTALL_PID
     EXIT_CODE=$?
 fi
